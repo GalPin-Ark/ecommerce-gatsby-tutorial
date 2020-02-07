aff17bd1b82b949da0e5b23182d359cb73141ec3
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-02-07 12:07:15 -0600

    update

--- src/components/Products/SkuCard.js
+++ src/components/Products/SkuCard.js
@@ -4,29 +4,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-const cardStyles = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-around',
-  alignItems: 'flex-start',
-  padding: '1rem',
-  marginBottom: '1rem',
-  boxShadow: '5px 5px 25px 0 rgba(46,61,73,.2)',
-  backgroundColor: '#fff',
-  borderRadius: '6px',
-  maxWidth: '300px',
-}
-const buttonStyles = {
-  fontSize: '13px',
-  textAlign: 'center',
-  color: '#fff',
-  outline: 'none',
-  padding: '12px',
-  boxShadow: '2px 5px 10px rgba(0,0,0,.1)',
-  backgroundColor: 'rgb(255, 178, 56)',
-  borderRadius: '6px',
-  letterSpacing: '1.5px',
-}
+
 
 const formatPrice = (amount, currency) => {
   let price = (amount / 100).toFixed(2)
--- src/components/advancedCheckout.js
+++ src/components/advancedCheckout.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
-const buttonStyles = {
-  fontSize: '13px',
-  textAlign: 'center',
-  color: '#fff',
-  outline: 'none',
-  padding: '12px 60px',
-  boxShadow: '2px 5px 10px rgba(0,0,0,.1)',
-  backgroundColor: 'rgb(255, 178, 56)',
-  borderRadius: '6px',
-  letterSpacing: '1.5px',
-}
+
 
 const Checkout = class extends React.Component {
   // Initialise Stripe.js with your publishable key.
