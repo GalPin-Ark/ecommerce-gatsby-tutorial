2b4156382e9d7d3882b19c509309cf06edfd84d5
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-02-14 17:01:41 -0600

    navbar update

--- src/components/Products/SkuCard.js
+++ src/components/Products/SkuCard.js
@@ -7,55 +7,55 @@ import Typography from '@material-ui/core/Typography';
 
 
 const formatPrice = (amount, currency) => {
-  let price = (amount / 100).toFixed(2)
-  let numberFormat = new Intl.NumberFormat(['en-US'], {
-    style: 'currency',
-    currency: currency,
-    currencyDisplay: 'symbol',
-  })
-  return numberFormat.format(price)
+    let price = (amount / 100).toFixed(2)
+    let numberFormat = new Intl.NumberFormat(['en-US'], {
+        style: 'currency',
+        currency: currency,
+        currencyDisplay: 'symbol',
+    })
+    return numberFormat.format(price)
 }
 
 const SkuCard = class extends React.Component {
-  state = {
-    disabled: false,
-    buttonText: 'ADD TO CART',
-    paymentMessage: '',
-  }
+    state = {
+        disabled: false,
+        buttonText: 'ADD TO CART',
+        paymentMessage: '',
+    }
 
-  resetButton() {
-    this.setState({ disabled: false, buttonText: 'ADD ME BABY ONE MORE TIME!' })
-  }
+    resetButton() {
+        this.setState({ disabled: false, buttonText: 'ADD ME BABY ONE MORE TIME!' })
+    }
 
-  addToCart(event, skuId, quantity = 1) {
-    event.preventDefault()
-    this.setState({ disabled: true, buttonText: 'ADDED...' })
-    this.props.addToCart(skuId)
-    setTimeout(this.resetButton.bind(this), 500)
-  }
+    addToCart(event, skuId, quantity = 1) {
+        event.preventDefault()
+        this.setState({ disabled: true, buttonText: 'ADDED...' })
+        this.props.addToCart(skuId)
+        setTimeout(this.resetButton.bind(this), 500)
+    }
 
-  render() {
-    const sku = this.props.sku
-    return (
+    render() {
+        const sku = this.props.sku
+        return (
 
-    <Card >
-      <CardContent>
-       
-        <Typography variant="h5" component="h2">
-        {sku.attributes.name}
-        </Typography>
-        
-        <Typography variant="body2" component="p">
-        Price: {formatPrice(sku.price, sku.currency)}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" color="primary" onClick={event => this.addToCart(event, sku.id)} disabled={this.state.disabled} size="small">{this.state.buttonText}<span className="material-icons">add_shopping_cart</span></Button>
-      </CardActions>
-      {this.state.paymentMessage}
-    </Card>
-    )
-  }
+            <Card>
+            <CardContent >
+
+            < Typography variant = "h5"
+            component = "h2" > { sku.attributes.name } </Typography>
+
+            <Typography variant = "body2"
+            component = "p" >
+            Price: { formatPrice(sku.price, sku.currency) } </Typography> </CardContent>
+             <CardActions >
+            <Button variant = "contained"
+            color = "primary"
+            onClick = { event => this.addToCart(event, sku.id) }
+            disabled = { this.state.disabled }
+            size = "small" > { this.state.buttonText } < span className = "material-icons" > add_shopping_cart </span></Button >
+            </CardActions> { this.state.paymentMessage } </Card>
+        )
+    }
 }
 
 export default SkuCard
---
+++ src/pages/skius.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import SkuCard from '../components/Products/SkuCard'
+import Grid from '@material-ui/core/Grid';
+const conatinerStyles = {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: '1rem 0 1rem 0',
+  }
+  const styGrid = {
+    flexWrap: 'wrap',
+    padding: '1rem',
+  }
+const stripe_url = 'https://api.stripe.com/'
+
+const Skius = class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            error: false,
+            fetchedData: [],
+        }
+      }
+    
+    componentDidMount() {
+
+        this.getProducts();
+    }
+
+    getProducts() {
+        fetch(stripe_url + "/v1/skus", {
+            method: "GET",
+            headers: { 'Authorization': `Bearer ${process.env.STRIPE_SECRET_KEY}` }
+        }).then(response => {
+            return response.json()
+        }).then(json => {
+            console.log(json)
+            this.setState({
+                fetchedData: json.data
+            })
+
+        });
+    }
+
+    render() {
+        const props = this.props;
+        const { fetchedData, loading } = this.state
+        return ( <div>
+            <Grid style={conatinerStyles}>
+             {fetchedData.map(sku => <Grid item xs={6} sm={4} style={styGrid}>
+            <SkuCard {...props} key={sku.id} sku={sku} />
+          </Grid>)}   
+            
+         
+          
+        </Grid>
+            
+            </div>
+
+        )
+    }
+}
+
+export default Skius
